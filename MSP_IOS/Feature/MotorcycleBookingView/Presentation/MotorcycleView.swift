fc5f680f1ddd567ca536454eafb1aae7f14edb12
PDungz <phungvandung03062003@gmail.com>
2025-10-25 17:29:18 +0700

    feat(MotorcycleView): implement UI in motorcycle view

---
+++ MSP_IOS/Feature/MotorcycleBookingView/Presentation/MotorcycleView.swift
@@ -0,0 +1,18 @@
+//
+//  MotorcycleView.swift
+//  MSP_IOS
+//
+//  Created by Phùng Văn Dũng on 24/10/25.
+//
+
+import SwiftUI
+
+struct MotorcycleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotorcycleView()
+}
